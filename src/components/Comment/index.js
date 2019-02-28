09a3c411c972aa878afcb384ac9c206e63f245a2
Jinxing Lin <172601673@qq.com>
2019-02-28 11:54:22 +0800

    推文评论图片

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import UserInfo from '@/components/UserInfo';
+import Image from '@/components/Image';
 import styles from './styles.css';
 import likeIcon from '@/assets/ic_tweet_detail_comment_like_n@svg.svg';
 import moreIcon from '@/assets/ic_tweet_detail_comment_more_n@svg.svg';
 
 class Comment extends React.Component {
   render() {
-    const { content, likeNum, replyNum } = this.props;
+    const { content, likeNum, replyNum, image } = this.props;
     return (
       <div className={`${styles.wrap} clearfix`}>
         <UserInfo
@@ -20,6 +21,14 @@ class Comment extends React.Component {
         />
         <div className={styles.commentWrap}>
           <p className={styles.comment}>{content}</p>
+          <Image
+            src={image}
+            style={{
+              height: '1.8rem',
+              width: '1.8rem',
+              margin: '0',
+            }}
+          />
           <div className={styles.actionWrap}>
             {replyNum ? <div className={`${styles.reply} clearfix`}>{replyNum}条回复</div> : null}
             <div className={styles.like}>
